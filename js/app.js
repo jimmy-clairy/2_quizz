9d87f81f9c29691f477add8c3bcbf58dfe13744d
Jimmy Clairy <jimcl@hotmail.fr>
2023-05-21 17:24:48 +0200

    Start modif with db quiz

--- js/app.js
+++ js/app.js
@@ -1,12 +1,71 @@
-const responses = ["b", "a", "b", "a", "c", "b", "c", "b", "b", "a"];
-const emojis = ["👎", "😭", "👀", "✨", "✔️", "🎉"];
-const color = ["red", "orange", "purple", "blue", "yellowgreen", "green"];
+async function fetchData() {
+    const res = await fetch("../db/data.json")
+    if (res.ok) {
+        return await res.json()
+    }
+    throw new Error("Impossible de contactez le serveur")
+}
+
+const dataQuestions = await fetchData()
+console.log(dataQuestions);
+
+const numberOfQuestion = Number(prompt("Nombre question max 10"))
+/** Create Number Random */
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+console.log(dataQuestions.length);
+console.log(getRandomInt(dataQuestions.length));
+const goodResponses = []
+
+const quizForm = document.getElementById("quiz-form")
+
+let numberQuestion = 1
+for (let i = 0; i < numberOfQuestion; i++) {
+    const randomQuestion = getRandomInt(dataQuestions.length)
+
+
+    const questionBlock = document.createElement("div")
+    questionBlock.setAttribute("class", "question-block")
+    const questionTitle = document.createElement("h2")
+    questionTitle.innerText = dataQuestions[randomQuestion].question
+
+    quizForm.append(questionBlock)
+    questionBlock.append(questionTitle)
+
+    /** Create input */
+    let numberResponse = 1
+    for (const [key, value] of Object.entries(dataQuestions[randomQuestion].response)) {
+        const responseBlock = document.createElement("div")
+
+        const inputResponse = document.createElement("input")
+        inputResponse.setAttribute("type", "radio")
+        inputResponse.setAttribute("id", `q${numberQuestion}-${numberResponse}`)
+        inputResponse.setAttribute("name", `q${numberQuestion}`)
+        inputResponse.setAttribute("value", key)
+        if (numberResponse === 1) {
+            inputResponse.setAttribute("checked", "")
+        }
+
+        const labelResponse = document.createElement("label")
+        labelResponse.setAttribute("for", `q${numberQuestion}-${numberResponse}`)
+        labelResponse.innerText = value
+
+        questionBlock.append(responseBlock)
+        responseBlock.append(inputResponse, labelResponse)
+
+        numberResponse++
+    }
+    goodResponses.push(dataQuestions[randomQuestion].goodResponse)
+
+    numberQuestion++
+}
 
 const form = document.querySelector('form')
-const questionBlock = document.querySelectorAll('.question-block')
-
 form.addEventListener('submit', (e) => {
     e.preventDefault()
+    console.log(resultQuestion());
+    console.log(goodResponses);
     compareResponse()
 })
 
@@ -17,20 +76,24 @@ function resultQuestion() {
     return results
 }
 
+// const emojis = ["👎", "😭", "👀", "✨", "✔️", "🎉"];
+// const color = ["red", "orange", "purple", "blue", "yellowgreen", "green"];
+
 function compareResponse() {
     const results = resultQuestion()
+    const questionBlocks = document.querySelectorAll(".question-block")
     let i = 0;
     let score = 0;
 
     for (const result of results) {
 
-        if (result === responses[i]) {
-            questionBlock[i].classList.remove("wrongResponse")
-            questionBlock[i].classList.add("goodResponse")
+        if (result === goodResponses[i]) {
+            questionBlocks[i].classList.remove("wrongResponse")
+            questionBlocks[i].classList.add("goodResponse")
             score++
         } else {
-            questionBlock[i].classList.remove("goodResponse")
-            questionBlock[i].classList.add("wrongResponse")
+            questionBlocks[i].classList.remove("goodResponse")
+            questionBlocks[i].classList.add("wrongResponse")
         }
         i++
     }
@@ -40,11 +103,8 @@ function compareResponse() {
 const [titleResult, mark] = document.querySelectorAll('.results p')
 
 function showResult(score) {
-    let scoreMore5 = (Math.round(score / 2))
-
-    titleResult.textContent = score === responses.length ? '🥳 Youpi 🥳' : 'Retentez votre chance'
-    mark.textContent = `${emojis[scoreMore5]} Vous avez ${score}/${responses.length} bonne reponse ${emojis[scoreMore5]}`
-    mark.style.color = color.at(scoreMore5)
+    titleResult.textContent = score === goodResponses.length ? '🥳 Youpi 🥳' : 'Retentez votre chance'
+    mark.textContent = `Vous avez ${score}/${goodResponses.length} bonne reponse`
 }
 
 // RESET COLOR BACKGROUND
